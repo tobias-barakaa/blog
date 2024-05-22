84e1aa01ebf761195c5d775e4ea8126ea1851982
tobias-barakaa <tobiasbarakan@gmail.com>
2024-05-22 16:39:59 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
 
 export default function App() {
 return (
@@ -8,6 +10,8 @@ return (
 <Routes>
 <Route path="/" element={<Home />} />
 <Route path="/about" element={<About />} />
+<Route path='/sign-up' element={<Signup />} />
+<Route path="/dashboard" element={<Dashboard />} />
 </Routes>
 </BrowserRouter>
 )
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      sugbube
+    </div>
+  )
+}
+
+export default Signin

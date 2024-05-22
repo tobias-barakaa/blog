802773a9e34c81441655eeb0000fe0261cfd2b7a
tobias-barakaa <tobiasbarakan@gmail.com>
2024-05-22 21:30:52 +0300

    created user model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';    
 
+dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.listen(3001, () => {
+mongoose.connect(process.env.MONGO).then(() => {
+console.log('Connected to MongoDBf');
+}).catch((err) => {
+console.log('Error: Could not connect to MongoDB');
+});
+
+app.listen(3000, () => {
 console.log('server running on port 3000jdkfj');
 });

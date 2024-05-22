58f47e73225662075b6aa35d091f1f5adf258b5e
tobias-barakaa <tobiasbarakan@gmail.com>
2024-05-22 18:59:13 +0300

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Button, Navbar, TextInput } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+    const path = useLocation().pathname;
   return (
     <div>
       <Navbar className="border-b-2">
@@ -31,11 +32,29 @@ const Header = () => {
                 <FaMoon />
             </Button>
         <Link to="sign-in">
-            <Button gradientDuoTone='purpleToBlue' pill>
+            <Button gradientDuoTone='purpleToBlue' outline pill>
                 Sign In
             </Button>
+            <Navbar.Toggle />
         </Link>
         </div>
+        <Navbar.Collapse>
+            <Navbar.Link active={path === "/"} as={'div'}>
+                <Link to='/'>
+                    Home
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/about'}>
+                <Link to='/about'>
+                    About
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/dashboard'} as={'div'}>
+                <Link to='/projects'>
+                    Projects
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
         
 
       </Navbar>

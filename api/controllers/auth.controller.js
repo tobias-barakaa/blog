9e020a4efdb91e2b01e4c9af19f84216cf1c02b5
tobias-barakaa <tobiasbarakan@gmail.com>
2024-05-23 19:11:13 +0300

    create login system api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,65 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
-export const signup = async(req, res, next) => { 
+export const signup = async (req, res, next) => { 
     const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === "" || email === "" || password === "") {
-        next(errorHandler(400, "All fields are required"))
+    // Validate input
+    if (!username || !email || !password || username === "" || email === "" || password === "") {
+        return next(errorHandler(400, "All fields are required"));
     }
 
+    // Hash the password
     const hashedPassword = bcrypt.hashSync(password, 10);
+
+    // Create new user with hashed password
     const newUser = new User({
         username,
         email,
-        password
+        password: hashedPassword  // Save the hashed password
     });
 
     try {
-
-    await newUser.save();
-    res.json({ message: "Signup success"})
-        
+        await newUser.save();
+        res.json({ message: "Signup success" });
     } catch (error) {
-        next(error)
-        
+        next(error);
+    }
+};
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+        return next(errorHandler(400, "All fields are required"));
     }
 
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(400, "User Not found"));
+        }
+
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid details"));
+        }
+
+        const token = jwt.sign({ id: validUser._id },
+            process.env.JWT_SECRET, { expiresIn: "1d" });
+        const { password: userPassword, ...user } = validUser._doc;
+
+        res.status(200)
+        .cookie('access_token', token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        }).json(user);
+
+    } catch (error) {
+        next(error);
+    }
 }
